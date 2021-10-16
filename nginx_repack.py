0024e8d1e6e8ca52ce477f130233ca8d01cf8fbd
troll <troll75677@gmail.com>
2021-10-16 17:28:51 +0300

    Start

---
+++ nginx_repack.py
@@ -0,0 +1,42 @@
+import re, os, sys
+
+results = []
+start_path = []
+BASE_PATH = os.getcwd()
+NGINX_PATH = '/etc/nginx/sites-enabled'
+
+# ResrMsk_domens.txt - перечень доменов
+# RestMsk_nginx.conf - конфигурационный файл nginx
+# template.txt - шаблон для записи конфигурации в nginx
+
+os.system('systemctl stop nginx.service')
+p_name = sys.argv[1]
+
+with open(BASE_PATH + f'/{p_name}_domens.txt', 'r') as domens,\
+    open(NGINX_PATH + r'/RestMO1_nginx.conf', 'r') as conf_file:
+    domens = domens.readlines()
+    finish_row = conf_file.readline()
+    while finish_row != '# block_base_conf_finish\n':
+        start_path.append(finish_row)
+        finish_row = conf_file.readline()
+    finish_path = conf_file.readlines()
+
+    with open(BASE_PATH + r'/template.txt', 'r') as nginx_temp:
+        nginx_temp = nginx_temp.read()
+        for domen in domens:
+            domen = domen.strip()
+            result = re.sub(r'project_name', sys.argv[1], nginx_temp)
+            result2 = re.sub(r'name_host', domen, result)
+            results.append(result2 + '\n')
+
+with open(NGINX_PATH + r'/default', 'w') as conf_file:
+    conf_file.write(''.join(start_path) + ''.join(results)\
+                     + '\n' + finish_row + ''.join(finish_path))
+
+os.system('systemctl start nginx.service')
+
+"""
+    1) Нужно создать ветвление, чтобы при отсутствии файла nginx в sites-enabled
+    этот файл создавался по шаблону.
+    2) Создать отдельную папку для всех шаблонов.
+"""

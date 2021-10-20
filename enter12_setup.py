fd49df85e570d56d10e4d6da4fe5a41fc5e6bd42
troll <troll75677@gmail.com>
2021-10-20 07:33:24 +0300

    add enter12 setup

---
+++ enter12_setup.py
@@ -0,0 +1,13 @@
+import re, os, sys
+
+p_name = sys.argv[1]
+URLS_PATH = f'/opt/projects/{p_name}/{p_name}/{p_name}'
+
+with open(URLS_PATH + '/urls.py', 'r') as urls_r:
+    lines = urls_r.readlines()
+    for line in lines:
+        if 'admin/' in line:
+            line = re.sub('admin/', 'enter12/', line)
+
+with open(URLS_PATH + '/urls.py', 'w') as urls_w:
+    urls_r.write(''.join(lines))

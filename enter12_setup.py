5d954f55a2376a076aef9c0007105d388dbff1c4
troll <troll75677@gmail.com>
2021-10-20 07:35:58 +0300

    upgrade urls_w

--- enter12_setup.py
+++ enter12_setup.py
@@ -6,8 +6,8 @@ URLS_PATH = f'/opt/projects/{p_name}/{p_name}/{p_name}'
 with open(URLS_PATH + '/urls.py', 'r') as urls_r:
     lines = urls_r.readlines()
     for line in lines:
-        if 'admin/' in line:
+        if '\'admin/' in line:
             line = re.sub('admin/', 'enter12/', line)
 
 with open(URLS_PATH + '/urls.py', 'w') as urls_w:
-    urls_r.write(''.join(lines))
+    urls_w.write(''.join(lines))

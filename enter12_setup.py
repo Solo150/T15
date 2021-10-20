a71690165f155f3a8ba3d5b4e4704fe814405af2
troll <troll75677@gmail.com>
2021-10-20 07:43:27 +0300

    upgrade enter12 setup finish

--- enter12_setup.py
+++ enter12_setup.py
@@ -4,10 +4,8 @@ p_name = sys.argv[1]
 URLS_PATH = f'/opt/projects/{p_name}/{p_name}/{p_name}'
 
 with open(URLS_PATH + '/urls.py', 'r') as urls_r:
-    lines = urls_r.readlines()
-    for line in lines:
-        if '\'admin/' in line:
-            line = re.sub('admin/', 'enter12/', line)
+    lines = urls_r.read()
+    line = re.sub('\'admin/', '\'enter12/', lines)
 
 with open(URLS_PATH + '/urls.py', 'w') as urls_w:
-    urls_w.write(''.join(lines))
+    urls_w.write(line)
